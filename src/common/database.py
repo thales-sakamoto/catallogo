ece38deb577aa6a534e329d47c9b694844599b14
Thales Sakamoto <thales.sakamoto@gmail.com>
2017-11-12 17:01:09 -0200

    second commit

--- src/common/database.py
+++ src/common/database.py
@@ -1,14 +1,14 @@
 import pymongo
-
+import os
 
 class Database(object):
-    URI = "mongodb://127.0.0.1:27017"
+    URI = os.environ.get("MONGODB_URI")
     DATABASE = None
 
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['fullstack']
+        Database.DATABASE = client.get_default_database()
 
     @staticmethod
     def insert(collection, data):
